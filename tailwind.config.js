0de48bc91a5be56a75e230fadfd2bede38e2a3cb
Dylan Ullrich <dylan@eldyl.org>
2025-03-06 21:28:13 -0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.rs",  // Important: include your Rust files
+    "./src/**/*.html",
+    "./style/**/*.css",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
